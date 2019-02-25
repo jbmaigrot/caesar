ec7250eaf1a687b89fd9a314d98fe865d49ff2fa
phenixapoil <35372381+phenixapoil@users.noreply.github.com>
2019-02-25 12:45:16 +0100

    Protochat working

---
+++ Assets/Scripts/ScriptChatBox.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScriptChatBox : MonoBehaviour
+{
+
+    static public string NewChatContent;
+    private string ChatContent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NewChatContent = "";
+        ChatContent = "\n";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (NewChatContent != "")
+        {
+            ChatContent += NewChatContent;
+            NewChatContent = "";
+        }
+        this.GetComponent<Text>().text = ChatContent;
+    }
+}
---
+++ Assets/Scripts/ScriptWritingPrompt.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScriptWritingPrompt : MonoBehaviour
+{
+    private string currentString;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentString = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (char c in Input.inputString)
+        {
+            if (c == '\b') // has backspace/delete been pressed?
+            {
+                if (currentString.Length != 0)
+                {
+                    currentString = currentString.Substring(0, currentString.Length - 1);
+                }
+            }
+            else if ((c == '\n') || (c == '\r')) // enter/return
+            {
+                if (currentString != "")
+                {
+                    ScriptChatBox.NewChatContent += currentString + "\n";
+                    currentString = "";
+                }
+                               
+            }
+            else
+            {
+                currentString += c;
+            }
+        }
+        this.GetComponent<Text>().text  = currentString;
+    }
+}
