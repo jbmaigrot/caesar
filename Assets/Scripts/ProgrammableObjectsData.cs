79e12f5f1990a93e73355e1ba58f6e379eafc69e
phenixapoil <35372381+phenixapoil@users.noreply.github.com>
2019-02-25 17:07:28 +0100

    Fini pour aujourd'hui

---
+++ Assets/Scripts/ProgrammableObjectsData.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Typeinput {OnHack, OnReadWord};
+
+public enum Typeoutput {Repeat, SendMessage, TurnLightOn, TurnLightOff};
+
+public struct Hackarrow
+{
+    public int target;
+    public float waitTime;
+};
+
+public struct Hackinput
+{
+    public Typeinput typeOfInput;
+    public string parameter;
+    public List<Hackarrow> arrow;
+};
+
+public struct Hackoutput
+{
+    public Typeoutput typeOfOutput;
+    public string parameter;
+};
+
+public class ProgrammableObjectsData : MonoBehaviour
+{
+    List<Hackinput> hackinput;
+    List<Hackoutput> hackoutput;
+
+    private void Start()
+    {
+        Hackinput voiture;
+        voiture.typeOfInput = Typeinput.OnHack;
+        voiture.parameter = "";
+        voiture.arrow = null;
+        hackinput.Add(voiture);
+
+        Hackoutput velodrome;
+        velodrome.typeOfOutput = Typeoutput.TurnLightOn;
+        velodrome.parameter = "";
+
+        Hackoutput velodrome2;
+        velodrome2.typeOfOutput = Typeoutput.TurnLightOff;
+        velodrome2.parameter = "";
+
+        Hackoutput velodrome3;
+        velodrome3.typeOfOutput = Typeoutput.Repeat;
+        velodrome3.parameter = "";
+
+    }
+
+    private void Update()
+    {
+
+    }
+}
