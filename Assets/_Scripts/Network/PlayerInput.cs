dfb081612cf0ce8f2e9dcff1f55155d94f373fc0
jbmaigrot <jb.maigrot@neuf.fr>
2019-03-22 14:21:48 +0100

    proto - navmesh & camera

--- Assets/_Scripts/Network/PlayerInput.cs
+++ Assets/_Scripts/Network/PlayerInput.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
 
-public class GameCreator1 : MonoBehaviour
+public class PlayerInput : MonoBehaviour
 {
     public NetworkManager networkManager;
     
