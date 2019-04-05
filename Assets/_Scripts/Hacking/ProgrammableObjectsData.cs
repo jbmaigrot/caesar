7d942a59af1bd3b59751e62b6a9b83e1cf69b33f
Sylvain Legay <legay@lri.fr>
2019-04-05 16:50:32 +0200

    NavMeshUpdate

--- Assets/_Scripts/Hacking/ProgrammableObjectsData.cs
+++ Assets/_Scripts/Hacking/ProgrammableObjectsData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.AI;
 
 public class ProgrammableObjectsData : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class ProgrammableObjectsData : MonoBehaviour
 
     /*Server. Seulement coté serveur*/
     public GameObject Server;
+    public GameObject NavMeshSurface;
 
     /*Variables contenant le graphe de comportement de l'objet*/
     public List<InOutVignette> inputCodes=new List<InOutVignette>();
@@ -36,6 +38,8 @@ public class ProgrammableObjectsData : MonoBehaviour
         {
             OnOutput(ryan.code, ryan.parameter_string, ryan.parameter_int);
         }
+
+        NavMeshSurface = GameObject.Find("NavMesh");
     }
 
     /*Si l'objet est cliqué à distance suffisament courte, ouvre l'interface de hack. Cette fonction doit être adapté pour le réseau.*/
@@ -92,12 +96,13 @@ public class ProgrammableObjectsData : MonoBehaviour
         if(codeoutput == "OpenDoor")
         {
             this.GetComponentInChildren<DoorScript>().OnOpen();
-
+            NavMeshSurface.GetComponent<NavMeshSurfaceScript>().hasToBeRebake = true;
         }
 
         if(codeoutput == "CloseDoor")
         {
             this.GetComponentInChildren<DoorScript>().OnClose();
+            NavMeshSurface.GetComponent<NavMeshSurfaceScript>().hasToBeRebake = true;
         }
 
         if(codeoutput == "SendMessage")/*A adapter pour le chat*/
