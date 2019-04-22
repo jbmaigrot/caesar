c850a07502856dc53e65935ed5cce31a2ba56b40
Phenix <phenixapoil@gmail.com>
2019-04-22 11:53:43 +0200

    littlefix

--- Assets/RosaceForHacking.cs
+++ Assets/RosaceForHacking.cs
@@ -11,7 +11,7 @@ public class RosaceForHacking : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         this.transform.position = Input.mousePosition;
 
