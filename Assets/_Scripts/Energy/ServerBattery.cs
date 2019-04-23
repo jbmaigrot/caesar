10b41cd14c28158f86632bc377f4977cf6860a2b
phenixapoil <jb.maigrot@gmail.com>
2019-04-23 11:01:43 +0200

    fixBattery

--- Assets/_Scripts/Energy/ServerBattery.cs
+++ Assets/_Scripts/Energy/ServerBattery.cs
@@ -26,8 +26,9 @@ public class ServerBattery : MonoBehaviour
         }
         foreach (Transform ryan in server.characters)
         {
-            if (ryan.GetComponent<ServerCarrier>().charge > 0 && (ryan.position - this.transform.position).magnitude < 30 && !server.players.Contains(ryan))
+            if (ryan.GetComponent<ServerCarrier>().charge > 0 && Vector3.Distance(ryan.position,this.transform.position) < 30 && !server.players.Contains(ryan))
             {
+                Debug.Log("Le volume");
                 ryan.GetComponent<NavMeshAgent>().destination = this.transform.position;
                 ryan.GetComponent<ServerCarrier>().StartGiving(this.GetComponent<ServerCarrier>());
             }
