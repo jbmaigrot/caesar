5aada9c985d574170138afe3315eb6efdfef10b9
phenixapoil <jb.maigrot@gmail.com>
2019-05-02 12:09:25 +0200

    fix source apparition

--- Assets/_Scripts/Energy/SourceManager.cs
+++ Assets/_Scripts/Energy/SourceManager.cs
@@ -8,8 +8,8 @@ public class SourceManager : MonoBehaviour
 
 #if SERVER
     public float timeBeforeNewSource = 180;
-    private float startingTime = 0;
-    private float timeBeforeSource;
+    public float startingTime = 0;
+    public float timeBeforeSource;
 #endif
 
     // Start is called before the first frame update
@@ -22,10 +22,10 @@ public class SourceManager : MonoBehaviour
         timeBeforeSource = 0;
         Random.InitState(System.DateTime.Now.Second);
 #endif
-        for (int i = 0; i < 3; i++)
+        /*for (int i = 0; i < 3; i++)
         {
             sources[i].gameObject.SetActive(false);
-        }
+        }*/
     }
 
     // Update is called once per frame
@@ -42,6 +42,7 @@ public class SourceManager : MonoBehaviour
             } while (sources[i].gameObject.activeSelf);
             sources[i].gameObject.SetActive(true);
             sources[i].startingTime = Time.time;
+            Debug.Log(i);
             timeBeforeSource = timeBeforeNewSource;
         }
         
