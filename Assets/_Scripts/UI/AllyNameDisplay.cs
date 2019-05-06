e828832787a98e84a83ff17bdd5ff5d3cf171bf0
Vovoda <vovoda@orange.fr>
2019-05-06 15:51:49 +0200

    Update drone_client prefab to display name
    ... in a better way (canvas and all). Paving the way to add a better charge display.

--- Assets/_Scripts/UI/AllyNameDisplay.cs
+++ Assets/_Scripts/UI/AllyNameDisplay.cs
@@ -1,25 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AllyNameDisplay : MonoBehaviour
 {
 #if CLIENT
     public CameraController cameraController;
-    public TextMesh allyNameText;
-    float baseCharacterSize;
+    public Text allyNameText;
+    float baseTextScale;
     // Start is called before the first frame update
     void Start()
     {
         cameraController = FindObjectOfType<CameraController>();
-        baseCharacterSize = allyNameText.characterSize;
+
+        baseTextScale = allyNameText.rectTransform.localScale.x;
+        Canvas canvas = this.GetComponent<Canvas>();
+        if (canvas != null)
+        {
+            GetComponent<RectTransform>().rotation = Quaternion.Euler(50.0f, -45.0f, 0.0f);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(transform.position + cameraController.transform.rotation * Vector3.forward, cameraController.transform.rotation * Vector3.up);
-        allyNameText.characterSize = baseCharacterSize * cameraController.zoomFactor;
+        //transform.LookAt(transform.position + cameraController.transform.rotation * Vector3.forward, cameraController.transform.rotation * Vector3.up);
+
+        allyNameText.GetComponent<RectTransform>().localScale = new Vector3(baseTextScale * cameraController.zoomFactor, baseTextScale * cameraController.zoomFactor, 1.0f);
     }
 #endif
 }
