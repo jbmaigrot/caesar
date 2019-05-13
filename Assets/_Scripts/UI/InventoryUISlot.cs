f51a2f90fabf9b2a884fe9b97878d9cf7617d00f
nadege <nadegepub@gmail.com>
2019-05-13 14:20:02 +0200

    Début Inventaire

---
+++ Assets/_Scripts/UI/InventoryUISlot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUISlot : MonoBehaviour
+{
+    public Sprite Normal;
+    public Sprite Attract;
+    public Sprite Stun;
+    public Sprite PowerPump;
+    public Sprite RedRelay;
+    public Sprite BlueRelay;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void reloadSlot()
+    {
+        
+    }
+}
