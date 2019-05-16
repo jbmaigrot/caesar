5cd7d44fbf17ecd411c159122c85732677d34590
phenixapoil <jb.maigrot@gmail.com>
2019-05-16 11:55:26 +0200

    Ajout de la chaise philosophe

--- Assets/_Scripts/Hacking/ProgrammableObjectsContainer.cs
+++ Assets/_Scripts/Hacking/ProgrammableObjectsContainer.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ProgrammableObjectsContainer : MonoBehaviour
 {
+    public ProgrammableObjectsScriptable PhilosopherChair;
 #if CLIENT
     public Client client;
     public List<ProgrammableObjectsData> objectListClient;
@@ -11,6 +12,7 @@ public class ProgrammableObjectsContainer : MonoBehaviour
 
 #if SERVER
     public List<ProgrammableObjectsData> objectListServer;
+    public List<ProgrammableObjectsData> chairListServer;
 #endif
 
     public void Start()
@@ -29,8 +31,42 @@ public class ProgrammableObjectsContainer : MonoBehaviour
 
 #if SERVER
             objectListServer.Add(ryan);
+            if (ryan.gameObject.name.Contains("Armchair"))
+            {
+                Debug.Log("its a chair");
+                chairListServer.Add(ryan);
+            }
 #endif
         }
+#if SERVER
+        float bestScore=0f;
+        ProgrammableObjectsData bestchair = new ProgrammableObjectsData();
+        float randScore;
+        foreach (ProgrammableObjectsData ryan in chairListServer)
+        {
+            Debug.Log("a true chair");
+            randScore = Random.Range(0f, 1f);
+            if (randScore > bestScore)
+            {
+                
+                bestchair = ryan;
+                bestScore = randScore;
+            }
+        }
+        bestchair.Initiator = Instantiate(PhilosopherChair);
+        bestchair.inputCodes = new List<InOutVignette>(bestchair.Initiator.inputCodes);
+        bestchair.outputCodes = new List<InOutVignette>(bestchair.Initiator.outputCodes);
+        bestchair.graph = new List<Arrow>(bestchair.Initiator.graph);
+        foreach (Arrow a in bestchair.graph)
+        {
+            a.timeBeforeTransmit.Clear();
+        }
+
+        foreach (InOutVignette ryan in bestchair.Initiator.initialOutputActions)
+        {
+            bestchair.OnOutput(ryan.code, ryan.parameter_string, ryan.parameter_int);
+        }
+#endif
     }
 
 #if SERVER
